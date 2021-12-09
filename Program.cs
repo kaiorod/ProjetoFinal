4307a8cc57b2b743f7a27988546362305f351544
kaiorod <caiorodrigo2775@gmail.com>
2021-12-09 12:27:31 -0300

    Finaliza ProjetoFinal

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ProjetoFinal
             menu:
             mold();//moldura
             
-            Console.SetCursorPosition(20,2);
+            Console.SetCursorPosition(20,1);
             Console.WriteLine("Escolha a Operação");
             Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.DarkCyan;
@@ -20,11 +20,11 @@ namespace ProjetoFinal
 
             Console.ResetColor();                             //|
             Console.SetCursorPosition(10,11);                 //|
-            Console.Write("Opção desejada : ");               //|-- Entrada Opção
-            Console.SetCursorPosition(27,11);                 //|
+            Console.Write("Opção desejada : ");               //|-- Entrada Opção              
             Console.ForegroundColor = ConsoleColor.DarkCyan;  //|
             op = int.Parse(Console.ReadLine());               //|
             Console.ResetColor();
+            
 
             switch(op)
             {
@@ -37,7 +37,7 @@ namespace ProjetoFinal
                 Console.Write("Deseja voltar ao menu? S/N : ");
                 string sn = Console.ReadLine().ToUpper();
                 if(sn == "S"){goto menu;}
-                else if(sn == "N"){Console.SetCursorPosition(0,12);}
+                else if(sn == "N"){creditos();}
                 else
                 {Console.ForegroundColor = ConsoleColor.DarkRed; 
                 Console.SetCursorPosition(27,10);
@@ -54,7 +54,7 @@ namespace ProjetoFinal
                 Console.Write("Deseja voltar ao menu? S/N : ");
                 string sn2 = Console.ReadLine().ToUpper();
                 if(sn2 == "S"){goto menu;}
-                else if(sn2 == "N"){Console.SetCursorPosition(0,12);}
+                else if(sn2 == "N"){creditos();}
                 else
                 {Console.ForegroundColor = ConsoleColor.DarkRed; 
                 Console.SetCursorPosition(27,10);
@@ -71,7 +71,7 @@ namespace ProjetoFinal
                 Console.Write("Deseja voltar ao menu? S/N : ");
                 string sn3 = Console.ReadLine().ToUpper();
                 if(sn3 == "S"){goto menu;}
-                else if(sn3 == "N"){Console.SetCursorPosition(0,12);}
+                else if(sn3 == "N"){creditos();}
                 else
                 {Console.ForegroundColor = ConsoleColor.DarkRed; 
                 Console.SetCursorPosition(27,10);
@@ -88,7 +88,7 @@ namespace ProjetoFinal
                 Console.Write("Deseja voltar ao menu? S/N : ");
                 string sn4 = Console.ReadLine().ToUpper();
                 if(sn4 == "S"){goto menu;}
-                else if(sn4 == "N"){Console.SetCursorPosition(0,12);}
+                else if(sn4 == "N"){creditos();;}
                 else
                 {Console.ForegroundColor = ConsoleColor.DarkRed; 
                 Console.SetCursorPosition(27,10);
@@ -105,7 +105,7 @@ namespace ProjetoFinal
                 Console.Write("Deseja voltar ao menu? S/N : ");
                 string sn5 = Console.ReadLine().ToUpper();
                 if(sn5 == "S"){goto menu;}
-                else if(sn5 == "N"){Console.SetCursorPosition(0,12);}
+                else if(sn5 == "N"){creditos();}
                 else
                 {Console.ForegroundColor = ConsoleColor.DarkRed; 
                 Console.SetCursorPosition(27,10);
@@ -122,7 +122,7 @@ namespace ProjetoFinal
                 Console.Write("Deseja voltar ao menu? S/N : ");
                 string sn6 = Console.ReadLine().ToUpper();
                 if(sn6 == "S"){goto menu;}
-                else if(sn6 == "N"){Console.SetCursorPosition(0,12);}
+                else if(sn6 == "N"){creditos();}
                 else
                 {Console.ForegroundColor = ConsoleColor.DarkRed; 
                 Console.SetCursorPosition(27,10);
@@ -139,61 +139,78 @@ namespace ProjetoFinal
                 Console.Write("Deseja voltar ao menu? S/N : ");
                 string sn7 = Console.ReadLine().ToUpper();
                 if(sn7 == "S"){goto menu;}
-                else if(sn7 == "N"){creditos();Console.SetCursorPosition(0,12);}
+                else if(sn7 == "N"){creditos();}
                 else
                 {Console.ForegroundColor = ConsoleColor.DarkRed; 
                 Console.SetCursorPosition(27,10);
                 Console.WriteLine("Opção Inválida"); 
                 Console.ResetColor(); goto sn7;}
                 break;
+
+                case 8:
+                Console.Clear();
+                creditos();
+                break;
+
+                default:
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.SetCursorPosition(27,10);
+                Console.WriteLine("INVÁLIDA");
+                Console.ResetColor();
+                Console.ReadKey();
+                goto menu;
+
             }
 
             
         }
         static void mold()//linha 1 até 12 - Moldura
         {
-            int c = 1;
-            while(c<=12)
+            int c = 0;
+            while(c <= 12)
             {
-                if(c ==1)
+                if(c == 0)
                 {
                     Console.SetCursorPosition(3,c);
-                    Console.WriteLine("╔════════════════════════════════════════════════╗");
+                    Console.WriteLine("╔════════════════════════════════════════════════════╗");
                 }
                 if(c == 12)
                 {
                     Console.SetCursorPosition(3,c);
-                    Console.WriteLine("╚════════════════════════════════════════════════╝");
+                    Console.WriteLine("╚════════════════════════════════════════════════════╝");
                 }
                 else
                 {
                     Console.SetCursorPosition(3,c+1);
-                    Console.WriteLine("║                                                ║");
+                    Console.WriteLine("║                                                    ║");
                 }
                 c++;
             }
         }
         static void menu()//linha 3 até 9 - Menu de Opções
         {
-            Console.SetCursorPosition(10,3);
+            Console.SetCursorPosition(10,2);
             Console.WriteLine("1. Soma");
-            Console.SetCursorPosition(10,4);
+            Console.SetCursorPosition(10,3);
             Console.WriteLine("2. Subtração");
-            Console.SetCursorPosition(10,5);
+            Console.SetCursorPosition(10,4);
             Console.WriteLine("3. Multipliacação");
-            Console.SetCursorPosition(10,6);
+            Console.SetCursorPosition(10,5);
             Console.WriteLine("4. Divisão");
-            Console.SetCursorPosition(10,7);
+            Console.SetCursorPosition(10,6);
             Console.WriteLine("5. Exponenciação");
-            Console.SetCursorPosition(10,8);
+            Console.SetCursorPosition(10,7);
             Console.WriteLine("6. Radiação");
-            Console.SetCursorPosition(10,9);
+            Console.SetCursorPosition(10,8);
             Console.WriteLine("7. Bháskara");
-            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(10,9);
+            Console.WriteLine("8. Créditos");
+            Console.ResetColor();
         }
         static void soma()
         {
-            Console.SetCursorPosition(20,2);
+            Console.SetCursorPosition(20,1);
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Soma '+' ---");
             Console.ResetColor();
@@ -212,7 +229,7 @@ namespace ProjetoFinal
         }
         static void sub()
         {
-            Console.SetCursorPosition(18,2);
+            Console.SetCursorPosition(18,1);
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Subtração '-' ---");
             Console.ResetColor();
@@ -229,7 +246,7 @@ namespace ProjetoFinal
         }
         static void mult()
         {
-            Console.SetCursorPosition(14,2);
+            Console.SetCursorPosition(14,1);
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Multiplicação 'X,*,.' ---");
             Console.ResetColor();
@@ -246,7 +263,7 @@ namespace ProjetoFinal
         }
         static void div()
         {
-            Console.SetCursorPosition(18,2);
+            Console.SetCursorPosition(18,1);
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Divisão ':,/' ---");
             Console.ResetColor();
@@ -263,7 +280,7 @@ namespace ProjetoFinal
         }
         static void exp()
         {
-            Console.SetCursorPosition(17,2);
+            Console.SetCursorPosition(17,1);
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Exponenciacão ---");
             Console.ResetColor();
@@ -280,7 +297,7 @@ namespace ProjetoFinal
         }
         static void rad()
         {
-            Console.SetCursorPosition(19,2);
+            Console.SetCursorPosition(19,1);
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Radiação ---");
             Console.ResetColor();
@@ -297,7 +314,7 @@ namespace ProjetoFinal
         }
         static void bhas()
         {
-            Console.SetCursorPosition(19,2);
+            Console.SetCursorPosition(19,1);
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Bháskara ---");
             Console.ResetColor();
@@ -324,7 +341,7 @@ namespace ProjetoFinal
         {
             Console.Clear();
             mold();
-            Console.SetCursorPosition(20,2);
+            Console.SetCursorPosition(20,1  );
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("--- Créditos ---");
             Console.ResetColor();
@@ -338,11 +355,11 @@ namespace ProjetoFinal
             Console.WriteLine("Ministrada pelos professores");
             Console.SetCursorPosition(4,7);
             Console.WriteLine("Diego Neri e Ermogenes Palácio");
-            Console.SetCursorPosition(20,8);
-            Console.WriteLine("Desenvolvido por Caio Rodrigo");
             Console.SetCursorPosition(20,9);
-            Console.WriteLine("Github : www.github.com/kaiorod");
+            Console.WriteLine("Desenvolvido por Caio Rodrigo");
             Console.SetCursorPosition(20,10);
+            Console.WriteLine("Github : www.github.com/kaiorod");
+            Console.SetCursorPosition(20,11);
             Console.WriteLine("Email : caiorodrigo2775@gmail.com");
 
             
